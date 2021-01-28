ae3fc83dcb65201940e80b89a8a4dc7efdc0a08f
Johar Alam <johar@glowlogix.com>
2021-01-28 16:58:16 +0500

    Job Status Update and Improvement

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -40,8 +40,69 @@
         elseif(!empty($candidate) && Auth::user()){
         $id = Candidate::where('job_name', $job->title)->where('user_id', Auth::user()->id)->first(); }
         $user = Auth::user();
+        $guest = Auth::guest();
       ?>
 
+      @if($job->status == '1')
+      <div class="w3-row w3-light-blue" style="margin-left: 370px; width: 731px;">
+        <!-- Blog entries -->
+        <div class="w3-col l8 s12">
+          <!-- Blog entry -->
+          <div class="w3-card-4 w3-margin w3-white" style="width: 700px;">
+
+            <div class="w3-margin" align="right">
+              @if($job->status == '1')
+                  <label align="center" title="opened" class="w3-green" style="height: 10%; width: 15%;">Job Opened</label>
+              @endif
+              @if($job->status == '0')
+                  <label align="center" title="closed" class="w3-red" style="height: 10%; width: 15%;">Job Closed</label>
+              @endif
+            </div>
+            <img src="{{ asset('storage/job.png') }}" alt="Nature" style="width:100%; height: 300px">
+            <div class="w3-container">
+              <h3><b>Title: <u>{{ $job->title }}</u></a></h3>
+              <h5>Department: <u>{{ $job->department}}</u></h5>
+              <h5>Enployment Type: <u>{{ $job->employment_type}}</u></h5>
+              <h5>Experience: <u>{{ $job->experience}}</u></h5>
+              <h5>Salary: <u>{{ $job->salary}}</u></h5>
+              <h5>Upload Date: {{ $job->created_at }}</h5>
+            </div><hr>
+
+            <div class="w3-container" class="teaser">
+              <p><b><u>Description:</u> {{  Str::limit($job->description, 100) }} {{-- Limit teaser to 100 characters --}}</b></p><hr>
+              <div class="w3-row" >
+                <div class="w3-col m14 s12">
+                  
+                  @if(!empty($id))
+                    <button title="Your are already applied for this job" class="btn btn-success" type="button" style="background-color: green;" disabled>Applied <i class="far fa-file-alt"></i></button>
+                  @endif
+                  @if(empty($id) || $user->name == 'Admin')
+                    <a href="{{ url('/jobs/view', $job->id) }}" title="View Job Details" class="btn btn-success" role="button"><i class="fa fa-eye" style="color:black;" ></i></a>
+                  @endif
+                  @canany(['Administer roles & permissions' , 'Show Candidates']) 
+                  <a href="{{ url('/jobs/appliedCandidates', $job->id) }}" title="View Applied Candidates" class="btn btn-info" role="button"><i class="fa fa-users" style="color: black;"></i></a>
+                  @endcanany
+                  @canany(['Administer roles & permissions' , 'Delete Job'])
+                  <a href="{{ url('/job/delete', $job->id) }}" title="Delete Job" class="btn btn-danger" role="button"><i class="fas fa-trash-alt" style="color: black;"></i></a>
+                  @endcanany
+                  @canany(['Administer roles & permissions' , 'Edit Job'])
+                  <a href="{{ url('/job/edit', $job->id) }}" title="Edit Job" class="btn btn-warning" role="button"><i class="fas fa-edit" style="color: black;"></i></a>
+                  @endcanany
+                </div>
+                @canany(['Administer roles & permissions' , 'Edit Job'])
+                <div align="center">
+                  <label > Status</label></br>
+                  <input data-id="{{$job->id}}" name="job_status" class="toggle-class" type="checkbox" data-onstyle="success" data-offstyle="danger" data-toggle="toggle" data-on="Open" data-off="Close" {{ $job->status ? 'checked' : ''}}>
+                </div>
+                @endcanany                            
+              </div></br>
+            </div>
+          </div>
+        </div>
+      </div></br>
+      @endif
+      @if($job->status == '0')
+      @can('Administer roles & permissions')
       <div class="w3-row w3-light-blue" style="margin-left: 370px; width: 731px;">
         <!-- Blog entries -->
         <div class="w3-col l8 s12">
@@ -98,6 +159,8 @@
           </div>
         </div>
       </div></br>
+      @endcan
+      @endif
     @endforeach
   </body>
   <script>
